67780275a212666901ba6d0aba0862d61d510775
baz3004 <japhetbazungula01@gmail.com>
2022-09-29 21:16:39 +0100

    crud admin joel

---
+++ database/migrations/2022_09_29_113912_create_calendriers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('calendriers', function (Blueprint $table) {
+            $table->id();
+            $table->string('activite');
+            $table->string('descrip')->nullable();
+            $table->date('debut');
+            $table->date('fin');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('calendriers');
+    }
+};
