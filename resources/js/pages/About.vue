c2b0d82fb02192eb05fb6debe39f077700ae29ef
pinocode21 <johnlutala@gmail.com>
2022-09-30 14:30:33 +0200

    after king push fixing footer and navbar

---
+++ resources/js/pages/About.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="flex w-full items-center justify-center py-8 bg-gray-100">
+        <div class="px-52">
+            <router-link to="/">Accueil</router-link>
+            <h1 class="text-white text-3xl font-serif font-bold">About</h1>
+        </div>
+        
+    </div>
+</template>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -12,7 +12,7 @@ const routes = [
     {
         name : 'home',
         path : '/',
-        component : Home
+        component : About
     },
     {
         name : 'actu',
