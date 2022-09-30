023f7397655966b85f4fe0e26b37c25e0156be18
pinocode21 <johnlutala@gmail.com>
2022-09-30 14:37:16 +0200

    npm install for to add splide

--- resources/js/pages/About.vue
+++ resources/js/pages/About.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="flex w-full items-center justify-center py-8 bg-gray-100">
+    <div class="flex w-full items-center justify-center py-8 bg-gray-100 mt-16">
         <div class="px-52">
             <router-link to="/">Accueil</router-link>
             <h1 class="text-white text-3xl font-serif font-bold">About</h1>
--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -12,7 +12,7 @@ const routes = [
     {
         name : 'home',
         path : '/',
-        component : About
+        component : Home
     },
     {
         name : 'actu',
