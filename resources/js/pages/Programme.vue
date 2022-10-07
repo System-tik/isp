68a22ccb5254828be1077d60c76de72fa1a7f123
king <glodin.kiese@gmail.com>
2022-10-07 09:32:15 +0100

    diki

--- resources/js/pages/Programme.vue
+++ resources/js/pages/Programme.vue
@@ -1,5 +1,43 @@
 <template>
-    <div>
-        programme
+    <div class="flex flex-col items-center justify-center w-full py-6 mt-20" style="background-color:#f4f6ff;">
+        <div class="">
+            <span class="text-sm"><router-link to="/" class="underline hover:text-blue-600">Accueil</router-link><span> / Frais</span></span>
+        </div>
+        <div class="mt-3">
+            <h1 class="text-3xl font-bold text-gray-900">ISP MBANZA-NGUNGU</h1>
+        </div>
+    </div>
+    <div class="px-4 lg:px-32 sm:px-8 md:px-16 xl:px-40 2xl:px-80">
+        <div >
+            <div class="text-xl sm:text-4xl text-slate-700 ">
+                Programme de cours
+            </div>
+            <div class="w-full pt-10 text-lg font-bold text-center sm:text-3xl text-slate-700">
+                Annee academique 2022-2023
+            </div>
+        </div>
+        <div>
+            <div class="py-8 text-gray-600 text-md sm:text-xl">
+                1. INFORMATIQUE ET TECHNOLOGIE
+            </div>
+            <div class="flex flex-col gap-1 ">
+                <div class="grid grid-cols-12 px-5 py-2 " style="background-color:#f4f6ff;">
+                    <div class="col-span-6 text-xs text-gray-600 sm:text-lg">
+                        CYCLE
+                    </div>
+                    <div class="col-span-6 text-xs text-gray-600 sm:text-lg">
+                        PROMOTION
+                    </div>
+                </div>
+                <div class="grid grid-cols-12 px-5 py-2 " style="background-color:#f4f6ff;">
+                    <div class="col-span-6 text-xs text-gray-600 sm:text-lg">
+                        LICENCE (LMD)
+                    </div>
+                    <div class="col-span-6 text-xs text-gray-600 sm:text-lg">
+                        PREMIERE LICENCE (L1)
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
